2a93247a53615730c75c359cc4a3c150268a0e09
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2026-03-25 11:38:39 +0000

    setup 1

---
+++ 01_TS-Setup-1/src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let number = 45;
+console.log(number);
+let name = "Rajik Alien";
+console.log(name);
+name = 96785;
+console.log(name);
+//# sourceMappingURL=index.js.map
---
+++ 01_TS-Setup-1/src/index.ts
@@ -0,0 +1,7 @@
+let number:number = 45
+console.log(number)
+
+let name:string = "Rajik Alien"
+console.log(name)
+name=96785
+console.log(name)
